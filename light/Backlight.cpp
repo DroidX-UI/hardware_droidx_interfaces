LineageOS/android_hardware_xiaomi

aidl: light: Rewrite backlight support
* This properly handles max brightness node

Change-Id: I8afcf4bb7cef9c71d9a03bac7e118b8d9cb0565b

aidl/light/Backlight.cpp
@@ -1,0 +1,85 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "Backlight.h"
+
+#include "LED.h"
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace light {
+
+class BacklightBrightness : public BacklightDevice {
+public:
+    BacklightBrightness(std::string name) : mBasePath(mkBacklightBasePath + name + "/") {
+        if (!readFromFile(mBasePath + "max_brightness", &mMaxBrightness)) {
+            mMaxBrightness = kDefaultMaxBrightness;
+        }
+    };
+
+    void setBacklight(uint8_t value) {
+        writeToFile(mBasePath + "brightness", value * mMaxBrightness / 0xFF);
+    }
+
+    bool exists() {
+        return fileWriteable(mBasePath + "brightness");
+    }
+private:
+    std::string mBasePath;
+    uint32_t mMaxBrightness;
+
+    inline static const std::string mkBacklightBasePath = "/sys/class/backlight/";
+    inline static const uint32_t kDefaultMaxBrightness = 255;
+};
+
+class LEDBacklight : public BacklightDevice {
+public:
+    LEDBacklight(std::string type) : mLED(type) {};
+
+    void setBacklight(uint8_t value) {
+        mLED.setBrightness(value);
+    }
+
+    bool exists() {
+        return mLED.exists();
+    }
+private:
+    LED mLED;
+};
+
+static const std::string kBacklightDevices[] = {
+    "panel0-backlight",
+};
+
+static const std::string kLedDevices[] = {
+    "lcd-backlight",
+};
+
+BacklightDevice *getBrightnessDevice() {
+    for (auto &device : kBacklightDevices) {
+        auto backlight = new BacklightBrightness(device);
+        if (backlight->exists()) {
+            return backlight;
+        }
+        delete backlight;
+    }
+
+    for (auto& device : kLedDevices) {
+        auto backlight = new LEDBacklight(device);
+        if (backlight->exists()) {
+            return backlight;
+        }
+        delete backlight;
+    }
+
+    return nullptr;
+}
+
+} // namespace light
+} // namespace hardware
+} // namespace android
+} // namespace aidl

aidl/light/Lights.cpp
@@ -15,11 +15,6 @@ namespace android {
 namespace hardware {
 namespace light {
 
-static const std::string kAllBacklightPaths[] = {
-    "/sys/class/backlight/panel0-backlight/brightness",
-    "/sys/class/leds/lcd-backlight/brightness",
-};
-
 static const std::string kAllButtonsPaths[] = {
     "/sys/class/leds/button-backlight/brightness",
     "/sys/class/leds/button-backlight1/brightness",
@@ -48,13 +43,9 @@ static const HwLight kButtonsHwLight = AutoHwLight(LightType::BUTTONS);
 static const HwLight kNotificationHwLight = AutoHwLight(LightType::NOTIFICATIONS);
 
 Lights::Lights() {
-    for (auto& backlight : kAllBacklightPaths) {
-        if (!fileWriteable(backlight))
-            continue;
-
-        mBacklightPath = backlight;
+    mBacklightDevice = getBacklightDevice();
+    if (mBacklightDevice) {
         mLights.push_back(kBacklightHwLight);
-        break;
     }
 
     for (auto& buttons : kAllButtonsPaths) {
@@ -79,8 +70,8 @@ ndk::ScopedAStatus Lights::setLightState(int32_t id, const HwLightState& state) 
     LightType type = static_cast<LightType>(id);
     switch (type) {
         case LightType::BACKLIGHT:
-            if (!mBacklightPath.empty())
-                writeToFile(mBacklightPath, color.toBrightness());
+            if (mBacklightDevice)
+                mBacklightDevice->setBrightness(color.toBrightness());
             break;
         case LightType::BUTTONS:
             for (auto& buttons : mButtonsPaths)

DroidX-UI/hardware_droidx_interfaces

Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

light/Backlight.cpp
@@ -1,0 +1,85 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "Backlight.h"
+
+#include "LED.h"
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace light {
+
+class BacklightBrightness : public BacklightDevice {
+public:
+    BacklightBrightness(std::string name) : mBasePath(mkBacklightBasePath + name + "/") {
+        if (!readFromFile(mBasePath + "max_brightness", &mMaxBrightness)) {
+            mMaxBrightness = kDefaultMaxBrightness;
+        }
+    };
+
+    void setBacklight(uint8_t value) {
+        writeToFile(mBasePath + "brightness", value * mMaxBrightness / 0xFF);
+    }
+
+    bool exists() {
+        return fileWriteable(mBasePath + "brightness");
+    }
+private:
+    std::string mBasePath;
+    uint32_t mMaxBrightness;
+
+    inline static const std::string mkBacklightBasePath = "/sys/class/backlight/";
+    inline static const uint32_t kDefaultMaxBrightness = 255;
+};
+
+class LEDBacklight : public BacklightDevice {
+public:
+    LEDBacklight(std::string type) : mLED(type) {};
+
+    void setBacklight(uint8_t value) {
+        mLED.setBrightness(value);
+    }
+
+    bool exists() {
+        return mLED.exists();
+    }
+private:
+    LED mLED;
+};
+
+static const std::string kBacklightDevices[] = {
+    "panel0-backlight",
+};
+
+static const std::string kLedDevices[] = {
+    "lcd-backlight",
+};
+
+BacklightDevice *getBacklightDevice() {
+    for (auto &device : kBacklightDevices) {
+        auto backlight = new BacklightBrightness(device);
+        if (backlight->exists()) {
+            return backlight;
+        }
+        delete backlight;
+    }
+
+    for (auto& device : kLedDevices) {
+        auto backlight = new LEDBacklight(device);
+        if (backlight->exists()) {
+            return backlight;
+        }
+        delete backlight;
+    }
+
+    return nullptr;
+}
+
+} // namespace light
+} // namespace hardware
+} // namespace android
+} // namespace aidl

light/Lights.cpp
@@ -15,11 +15,6 @@ namespace android {
 namespace hardware {
 namespace light {
 
-static const std::string kAllBacklightPaths[] = {
-    "/sys/class/backlight/panel0-backlight/brightness",
-    "/sys/class/leds/lcd-backlight/brightness",
-};
-
 static const std::string kAllButtonsPaths[] = {
     "/sys/class/leds/button-backlight/brightness",
     "/sys/class/leds/button-backlight1/brightness",
@@ -48,13 +43,9 @@ static const HwLight kButtonsHwLight = AutoHwLight(LightType::BUTTONS);
 static const HwLight kNotificationHwLight = AutoHwLight(LightType::NOTIFICATIONS);
 
 Lights::Lights() {
-    for (auto& backlight : kAllBacklightPaths) {
-        if (!fileWriteable(backlight))
-            continue;
-
-        mBacklightPath = backlight;
+    mBacklightDevice = getBacklightDevice();
+    if (mBacklightDevice) {
         mLights.push_back(kBacklightHwLight);
-        break;
     }
 
     for (auto& buttons : kAllButtonsPaths) {
@@ -80,8 +71,8 @@ ndk::ScopedAStatus Lights::setLightState(int32_t id, const HwLightState& state) 
     LightType type = static_cast<LightType>(id);
     switch (type) {
         case LightType::BACKLIGHT:
-            if (!mBacklightPath.empty())
-                writeToFile(mBacklightPath, color.toBrightness());
+            if (mBacklightDevice)
+                mBacklightDevice->setBacklight(color.toBrightness());
             break;
         case LightType::BUTTONS:
             for (auto& buttons : mButtonsPaths)